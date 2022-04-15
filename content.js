d80a08b74da9f09393e24d1f3c03c73a94697371
Jesús Cervantes Mac <jesus.cervantes081@gmail.com>
2022-04-14 23:32:46 -0500

    setup dom communication and key responding

--- content.js
+++ content.js
@@ -1,25 +1,41 @@
-console.log("Loaded content script.")
+console.log("Loaded content script.s")
 
 chrome.runtime.onMessage.addListener(
 	function (request, sender, sendResponse) {
-		console.log({ request })
 		if (request.greeting == "GET_DOM") {
-
-			const wrapperDiv = document.getElementsByClassName("flex flex-col container w-full max-w-lg pb-2 px-[10px] mx-auto gap-2")[0]
-
-			const rows = wrapperDiv.getElementsByTagName('div')
-
-			const elements = []
-
-			for (var i = 0; i < rows.length; i++) {
-				const buttons = rows[i].getElementsByTagName('button')
-				for (var j = 0; j < buttons.length; j++) {
-					elements.push(buttons[j])
-				}
-			}
-
-			elements[0].click()
-
-			console.log({ dom: elements })
+			sendResponse(
+				JSON.stringify(
+					document.getElementsByClassName("board grid grid-rows-6 relative gap-[5px] p-[10px] box-border max-w-[352.5px] max-h-[420px]")[0].outerHTML
+				)
+			)
 		}
 	})
+
+const writeWord = (word) => {
+	for (i = 0; i < word.length; i++) {
+		keyButton(word[i]).click();
+	}
+	submitWord()
+}
+
+const keyButton = (key) => {
+	const buttons = document.getElementsByClassName("flex-1 rounded uppercase font-bold p-1 sm:p-2 h-14 text-sm")
+
+	for (let i = 0; i < buttons.length; i++) {
+		if (buttons[i].ariaLabel === key) {
+			return buttons[i]
+		}
+	}
+}
+
+const submitWord = () => {
+	const buttons = document.getElementsByClassName("flex-1 rounded uppercase font-bold p-1 sm:p-2 h-14 text-sm")
+
+	console.log({ buttons })
+	for (let i = 0; i < buttons.length; i++) {
+		if (buttons[i].innerText === "ENVIAR") {
+			buttons[i].click()
+			return
+		}
+	}
+}
--- popup.js
+++ popup.js
@@ -2,6 +2,10 @@ console.log("Loaded popup script.")
 
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 	chrome.tabs.sendMessage(tabs[0].id, { greeting: "GET_DOM" }, function (response) {
-		console.log(response.farewell);
+		console.log(response)
+		console.log(JSON.parse(response))
+		document.getElementById("body").innerHTML = JSON.parse(response)
 	});
+
+	chrome.tabs.sendMessage(tabs[0].id, { wordToWrite: "" })
 });
